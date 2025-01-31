25cbd779b2e4a311688978b4d9599e3eefb57b16
Damian <day22482@gmail.com>
2025-01-31 14:37:21 -0500

    Adding new peojects

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,32 +1,41 @@
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 const Contact = () => {
   return (
     <div className="border-b border-neutral-900 pb-20">
-        <motion.div 
-        whileInView={{opacity: 1, x:0}}
-        initial={{opacity: 0, x:-500}}
-        transition={{duration: 0.75}}
-        className="border-t border-gray-600 mx-auto w-3/4"></motion.div>
-        <motion.h1 
-        whileInView={{opacity: 1, y:0}}
-        initial={{opacity: 0, y:200}}
-        transition={{duration: 0.75}}
-        className="mt-20 mb-5 text-center text-3xl sm:text-4xl lg:text-5xl font-light">Contact</motion.h1>
-        <div className="text-center tracking-tighter">
-            <motion.p 
-            whileInView={{opacity: 1, y:0}}
-            initial={{opacity: 0, y:125}}
-            transition={{duration: 0.75}}
-            className="py-4 text-xl">484-678-8595</motion.p>
-            <motion.p 
-            whileInView={{opacity: 1, y:0}}
-            initial={{opacity: 0, y:50}}
-            transition={{duration: 0.75}}
-            className="py-4 text-xl">day22482@gmail.com</motion.p>
-        </div>
-
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -500 }}
+        transition={{ duration: 0.75 }}
+        className="border-t border-gray-600 mx-auto w-3/4"
+      ></motion.div>
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: 200 }}
+        transition={{ duration: 0.75 }}
+        className="mt-20 mb-5 text-center text-3xl sm:text-4xl lg:text-5xl font-light"
+      >
+        Contact
+      </motion.h1>
+      <div className="text-center tracking-tighter">
+        <motion.p
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 125 }}
+          transition={{ duration: 0.75 }}
+          className="py-4 text-xl"
+        >
+          484-678-8595
+        </motion.p>
+        <motion.p
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 50 }}
+          transition={{ duration: 0.75 }}
+          className="py-4 text-xl"
+        >
+          day22482@gmail.com
+        </motion.p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
